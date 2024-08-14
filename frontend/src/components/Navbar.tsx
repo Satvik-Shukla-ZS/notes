abf2fec5e170c0df8da87866a3435e5a678159ec
Andrew <andrew.staines@zopsmart.com>
2024-08-14 12:20:15 +0530

    [ Feature ] : Fix : Added React-Icons and modified the component

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -5,6 +5,7 @@ import { googleLogout } from '@react-oauth/google'
 import { useNavigate } from 'react-router-dom'
 import logout from "../assets/img/logout.png";
 import data from './data';
+import { RiLogoutCircleLine } from "react-icons/ri";
 
 const Navbar = () => {
   const [profile, setProfile] = useState<RES_USER_PROFILE>()
@@ -29,10 +30,10 @@ const Navbar = () => {
       <nav className='w-full px-2 py-4 flex justify-between'>
         <input type="text" className='w-40 px-4 rounded-md p-2 border-slate-400 border-2' placeholder='Search Notes' />
         <div className='flex flex-row lg:gap-4 max-md:gap-4 md:gap-4 max-sm:gap-2'>
-          <div className='flex flex-row items-center justify-center'>
+          <div className='flex flex-row gap-2 items-center justify-center'>
             <img src={profile?.profile} className='rounded-full w-14 h-14 border-2 ' alt="pfp"/>
             <h1>{profile?profile.name:""}</h1>
-            <img src={logout} className='rounded-full w-8 h-8 border-2 ' alt="pfp" onClick={handleLogout} />
+            <RiLogoutCircleLine className='text-2xl cursor-pointer ' />
           </div>
         </div>
       </nav>
